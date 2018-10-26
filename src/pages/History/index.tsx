cda7cfb6b52735970fda681bc6d5e82878b3379b
zjc <710124011@qq.com>
2018-10-26 17:07:00 +0800

    10.26完成供应商商品的嵌套循环,以及小结的计算.食物的增减关联计算,

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 import { connect, MapDispatchToProps, MapStateToPropsParam } from 'react-redux'
-import { TabBar, List, Checkbox,InputItem , Flex } from 'antd-mobile'
+import { TabBar, List, Checkbox,InputItem , Flex ,Stepper } from 'antd-mobile'
 import { GlobalData } from '@store/reducers/globalDataReducer'
 import './default.css'
+import Head from '../../components/Head/index'
 
 const CheckboxItem = Checkbox.CheckboxItem
 const AgreeItem = Checkbox.AgreeItem
@@ -20,7 +21,8 @@ export interface Props {
 }
 
 interface State {
-
+  num: any,
+  data: any
 }
 
 class History extends React.Component<Props, State> {
@@ -28,6 +30,7 @@ class History extends React.Component<Props, State> {
   constructor (props) {
     super(props)
     this.state = {
+      num: '',
       data: [
         {
           value: 0,
@@ -38,56 +41,59 @@ class History extends React.Component<Props, State> {
             { value: 2, name: '精选有机红皮黄瓜',img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1540454284106&di=c0eadb783134644f2e577eb1fd7983f5&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2Feaf81a4c510fd9f9ba7fb76e2f2dd42a2834a449.jpg',price: '35.5',unit: '500g',count: '' }
           ]
         },
-        { value: 1, name: '大范德萨都是撒反对' },
-        { value: 2, name: '电话司法局看到回复撒' }
+        { value: 1, name: '大范德萨都是撒反对',foodList: [] },
+        { value: 2, name: '电话司法局看到回复撒',foodList: [] }
       ]
     }
   }
 
   /**
    * 供应商下的食物
+   * index-----食物下标,index1供应商下标
    */
-  renderFoodItem = () => {
-    const data = [
-      { value: 0, name: '精选有机红皮洋葱',img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1540454190461&di=4ec63d020893da20eb87e4913bf558fd&imgtype=0&src=http%3A%2F%2Fimg007.hc360.cn%2Fk2%2FM0E%2F5B%2FDB%2FwKhQxVidG2-Ea2fJAAAAAOyzV4A037.jpg',price: '15.5',unit: '500g',count: '' },
-      { value: 1, name: '精选有机红皮土豆',img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1540454256654&di=998aa4f7beaf2baff77e44379940ca2c&imgtype=0&src=http%3A%2F%2Fimg4.duitang.com%2Fuploads%2Fblog_extra%2F201408%2F27%2F20140827163148_8WrGv.jpeg',price: '25.5',unit: '500g',count: '' },
-      { value: 2, name: '精选有机红皮黄瓜',img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1540454284106&di=c0eadb783134644f2e577eb1fd7983f5&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2Feaf81a4c510fd9f9ba7fb76e2f2dd42a2834a449.jpg',price: '35.5',unit: '500g',count: '' }
-    ]
+  renderFoodItem = (item,index,index1) => {
     return (
-      <div>
-        {data.map(i => (
-          <div className='food' style={{ display: 'flex',alignItems: 'center' }} key={i.value}>
+      <div key={item.value}>
+        父级下标:{index1},子级下标:{index}
+          <div className='food' style={{ display: 'flex',alignItems: 'center' }}>
             <CheckboxItem style={{ width: '100%',background: 'transparent',height: 125 }}>
               <div style={{
                 display: 'flex',
                 alignItems: 'center'
               }}>
-                <img style={{ display: 'block',width: 90,height: 90 }} src={i.img} />
+                <img style={{ display: 'block',width: 90,height: 90 }} src={item.img} />
                 <div style={{ height: 105,display: 'flex',flexDirection: 'column' , justifyContent: 'space-between',paddingLeft: 20 }}>
-                  <div style={{ fontSize: '16px' }}>{i.name}</div>
+                  <div style={{ fontSize: '16px' }}>{item.name}</div>
                   <div style={{ fontSize: '16px' }}>
-                    <span style={{ color: 'red' }}>￥{i.price}</span>
-                    <span style={{ color: '#8c8c8c' }}>/{i.unit}</span>
+                    <span style={{ color: 'red' }}>￥{item.price}</span>
+                    <span style={{ color: '#8c8c8c' }}>/{item.unit}</span>
                   </div>
                   <div style={{ display: 'flex',color: '#8c8c8c',fontSize: 14 }}>
-                    <div style={{ display: 'flex',alignItems: 'center',justifyContent: 'center' ,width: 30,height: 30,border: '1px solid #8C8C8C' }} onClick={this.subtractOnClick}>-</div>
-                    <div style={{ display: 'flex',alignItems: 'center',justifyContent: 'center' }}><InputItem
-                      type='number'
-                      onChange={(v) => { i.count = v,console.log(i.count) }}
-                      onBlur={(v) => { console.log('onBlur', v) }}
-                      moneyKeyboardWrapProps={moneyKeyboardWrapProps}
-                      style={{
-                        border: '1px solid #8C8C8C',borderLeft: 0,borderRight: 0,width: 50,height: 30,textAlign: 'center'
-                      }}
-                    ></InputItem></div>
-                    <div style={{ display: 'flex',alignItems: 'center',justifyContent: 'center' ,width: 30,height: 30,border: '1px solid #8C8C8C' }} onClick={ () => { i.count += 1 } }>+</div>
-                    {i.count}
+                    <Stepper
+                      className='Stepper'
+                      showNumber
+                      max={10}
+                      min={1}
+                      defaultValue={3}
+                      onChange={ (v) => {
+                        let data = this.state.data
+                        data[index1].foodList[index].count = v
+                        this.setState({ data: data })
+                      } }
+                    />
                   </div>
                 </div>
               </div>
             </CheckboxItem>
+            数量是:{this.state.data[index1].foodList[index].count}
           </div>
-        ))}
+          <div style={{ display: 'flex',width: '100%' }}>
+            <div style={{ width: 161 }}></div>
+            <div style={{ display: 'flex',alignItems: 'center',height: 40,flex: 1,borderTop: '1px solid #e5e5e5' }}>
+              <div>小计: {this.state.data[index1].foodList[index].count * this.state.data[index1].foodList[index].price}</div>
+            </div>
+            <div style={{ width: 30 }}></div>
+          </div>
       </div>
     )
   }
@@ -95,16 +101,10 @@ class History extends React.Component<Props, State> {
   /**
    * 供应商
    */
-  renderSupplierItem = () => {
-    const data = [
-      { value: 0, name: '衢州炒菜软件有限公司' },
-      { value: 1, name: '大范德萨都是撒反对' },
-      { value: 2, name: '电话司法局看到回复撒' }
-    ]
+  renderSupplierItem = (i,index1) => {
     return (
       <div>
-        {data.map(i => (
-          <div key={i.value}>
+          <div >
             <div style={{ height: 15,background: '#f5f5f5' }}></div>
             <div style={{
               display: 'flex',
@@ -122,9 +122,8 @@ class History extends React.Component<Props, State> {
               alignItems: 'center'
             }}>
             </div>
-            {this.renderFoodItem()}
+            {i.foodList.map((item,index) => this.renderFoodItem(item,index,index1))}
           </div>
-        ))}
       </div>
     )
   }
@@ -143,7 +142,16 @@ class History extends React.Component<Props, State> {
   public render () {
     return (
       <div>
-        {this.renderSupplierItem()}
+        <Head title='菜篮子'></Head>
+        <div style={{ height: 40 }}></div>
+        {this.state.data.map((i,index1) => (
+          this.renderSupplierItem(i,index1)
+        ))}
+        <List>
+          <List.Item>123456</List.Item>
+          <List.Item>123456</List.Item>
+          <List.Item>123456</List.Item>
+        </List>
       </div>
     )
   }
