da193f75a171539667c85adb8df780f410adc7f2
bigHeadFish <947261129@qq.com>
2018-10-29 17:05:22 +0800

    首页跳转到 商品列表 商品列表页面设计

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import globalDataReducer from './globalDataReducer'
+import categoryItemDataReducer from './categoryItemDataReducer'
 
 const rootReducer = combineReducers<any>({
-  globalData: globalDataReducer as any
+  globalData: globalDataReducer as any,
+  categoryItemData: categoryItemDataReducer as any
 })
 
 export default rootReducer
