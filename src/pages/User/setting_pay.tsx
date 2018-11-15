97b1ba7f50dab676c165ecdea0f5af6eccb81147
ywy <631592452@qq.com>
2018-11-15 08:50:04 +0800

    2018/11/15 服务器宕机，所以提交  --by周

--- src/pages/User/setting_pay.tsx
+++ src/pages/User/setting_pay.tsx
@@ -21,9 +21,12 @@ interface State {
   orderChecked: boolean
   data: any
   ssTt: number
+  refresh: any
 }
 
 let offsetY: number = 0
+let layer: number = 0
+let top: number = 170
 class User extends React.Component<Props, State> {
 
   constructor (props) {
@@ -32,12 +35,14 @@ class User extends React.Component<Props, State> {
       checked: false,
       orderChecked: false,
       data: [
-        { id: '1', name: '账户余额', order: '1' },
-        { id: '2', name: '微信', order: '2' },
-        { id: '1', name: '账户余额', order: '3' },
-        { id: '2', name: '微信', order: '4' }
+        { id: '1', name: '账户余额', order: 1 },
+        { id: '2', name: '微信', order: 2 },
+        { id: '3', name: '支付宝', order: 3 },
+        { id: '4', name: '工商银行', order: 4 },
+        { id: '5', name: '建设银行', order: 5 }
       ],
-      ssTt: 0
+      ssTt: 0,
+      refresh: ''
     }
   }
 
@@ -124,27 +129,30 @@ class User extends React.Component<Props, State> {
           <span>拖动</span>
         </div>
         <div className='Segment_line2' />
-        <div style={{ paddingLeft: 20, marginTop: 10, fontSize: 14,color: '#8f8f8f' }}>
+        <div style={{ left: 32, fontSize: 14,color: '#8f8f8f',position: 'absolute',top: top + this.state.data.length * 40 + 50 }}>
           <span>对于特殊业务有特殊规则的，将遵循业务规则扣款</span>
         </div>
       </div>
     )
   }
+  /*
+  列表项的渲染
+  **/
   public renderItem = (i,index) => {
     return(
-      <div id={'main'} title={i.order} style={{ position: 'absolute', top: 300 + index * 40,width: '100%' }}
-           onTouchMove={this.touch}
-           onTouchStart={this.touchStart}
-           onTouchEnd={this.touchEnd}
+      <div id={'main'} title={i.order} style={{ position: 'absolute', top: top + i.order * 40,width: '100%',height: 40,backgroundColor: '#fff' }}
+           onTouchMove={this.touch.bind(this,i.order,index)}
+           onTouchStart={this.touchStart.bind(this,i.order,index)}
+           onTouchEnd={this.touchEnd.bind(this,i.order,index)}
       >
-        <div className='Segment_line' />
+        <div className={'Segment_line4'}/>
         <div style={{
           display: 'flex',
           justifyContent: 'space-between',
           flexDirection: 'row',
           padding: 10,
           backgroundColor: '#ffffff',
-          height: 20
+          height: 19
         }}>
           <span style={{ fontSize: '16px', marginLeft: 10 }}>{i.name}</span>
           <img src='../../assets/images/foot_home.svg' width='20' height='20' />
@@ -152,41 +160,80 @@ class User extends React.Component<Props, State> {
       </div>
     )
   }
-  public touch = (d) => {
-    if (d.target.offsetParent.id === 'main') {
-      d.target.offsetParent.style.top = (d.targetTouches['0'].clientY - offsetY + parseInt(d.target.offsetParent.style.top.substr(0,d.target.offsetParent.style.top.length - 2),0)) + 'px'
+  /*
+  主要功能： 列表拖拽（无动画）
+  核心思想：
+  1.通过order属性进行排序
+  2.当前项移动到的层赋值给当前项的order
+  3.找到目标层的索引
+  4.通过索引将当前项的原本所在层赋值给目标层的order
+  **/
+  public touch = (order,index,event) => { // 根据当前项的区间判断目标层
+    let currentTop: number = parseInt(event.target.offsetParent.style.top.substr(0,event.target.offsetParent.style.top.length - 2),0)
+    for (let i = 1;i <= this.state.data.length;i++) {  /* 遍历n次*/
+      if (currentTop < ((i + 1) * 40 + top - 20) && currentTop >= ((i) * 40 + top - 20)) {
+        layer = i
+      }
     }
-    offsetY = d.targetTouches['0'].clientY
-    if (parseInt(d.target.offsetParent.style.top.substr(0,d.target.offsetParent.style.top.length - 2),0) >= (parseInt(d.target.offsetParent.title,0) * 40 + 280)) {
-      console.log(d.target.offsetParent.offsetParent.children[2].children[parseInt(d.target.offsetParent.title,0)].children[0].className = 'a1')
+    if (event.target.offsetParent.id === 'main') {
+      event.target.offsetParent.style.top = (event.targetTouches['0'].clientY - offsetY + parseInt(event.target.offsetParent.style.top.substr(0,event.target.offsetParent.style.top.length - 2),0)) + 'px'
     }
-    /*
-    console.log(d.targetTouches['0'].clientX)
-    console.log(d.targetTouches['0'].clientY)
-    console.log(d.target.offsetParent)
-    console.log(d.targetTouches)
-    d.target.style.height = '10px'
-     */
+    offsetY = event.targetTouches['0'].clientY
+
   }
-  public touchStart = (d) => {
-    if (d.target.offsetParent.id === 'main') {
-      d.target.offsetParent.style.zIndex = 100
-      offsetY = d.targetTouches['0'].clientY
-      d.target.offsetParent.style.height = 40
-      d.target.offsetParent.children[0].title = ''
+  public touchStart = (name,index,event) => {
+    if (event.target.offsetParent.id === 'main') {
+      event.target.offsetParent.style.zIndex = 20
+      offsetY = event.targetTouches['0'].clientY
+      event.target.offsetParent.style.height = 40
+      event.target.offsetParent.children[0].className = ''
+    }
+    let currentTop: number = parseInt(event.target.offsetParent.style.top.substr(0,event.target.offsetParent.style.top.length - 2),0)
+    for (let i = 1;i <= this.state.data.length;i++) {  /* 遍历n次*/
+      if (currentTop < ((i + 1) * 40 + top - 20) && currentTop >= ((i) * 40 + top - 20)) {
+        layer = i
+      }
     }
     // d.target.style.height = '10px'
   }
-  public touchEnd = (d) => {
-    if (d.target.offsetParent.id === 'main') {
-      d.target.offsetParent.style.zIndex = parseInt(d.target.offsetParent.title,0)
-      d.target.offsetParent.children[0].className = 'Segment_line'
-      // d.target.offsetParent.className = 'a1'
+  public touchEnd = (order,index,event) => {
+    event.target.offsetParent.children[0].className = 'Segment_line4'
+    event.target.offsetParent.style.zIndex = 5
+    let currentTop: number = parseInt(event.target.offsetParent.style.top.substr(0,event.target.offsetParent.style.top.length - 2),0)
+    if (event.target.offsetParent.id === 'main') {
+      if (layer !== this.state.data[index].order && layer !== 0) {
+        for (let i = 0;i < this.state.data.length;i++) {  /* 遍历n次*/
+          if (this.state.data[i].order === layer) {
+            this.state.data[i].order = order
+          }
+        }
+        this.state.data[index].order = layer  // 拿着的正确
+        this.setState({
+          refresh: 'refresh'
+        })
+      }
+      console.log(currentTop)
+      if (currentTop <= top + 40) {
+        event.target.offsetParent.style.top = (top + 40) + 'px'
+        layer = 0
+        offsetY = 0
+        return
+      }
+      if (currentTop >= this.state.data.length * 40 + top) {
+        event.target.offsetParent.style.top = (this.state.data.length * 40 + top) + 'px'
+        layer = 0
+        offsetY = 0
+        return
+      }
+      console.log(layer)
+      event.target.offsetParent.style.top = (layer) * 40 + top + 'px'
+      layer = 0
+      offsetY = 0
     }
   }
   public render () {
     return (
-      <div className={'t'} style={{ position: 'relative',width: '100%', left: '100%' }}>
+      <div className={'t'} style={{ position: 'relative',width: '100%' }}>
         {this.renderNav()}
         {this.renderContent()}
         <div style={{ backgroundColor: '#ffffff' }}>
