3853a8fdae77157ae1999f9104a2404b1f7709ae
zjc <710124011@qq.com>
2018-11-27 10:28:51 +0800

    解决惯性滑动,ios下 fixed固定定位元素上下跳动的问题.

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -513,13 +513,15 @@ class History extends React.Component<Props, State> {
     return (
       <div>
         <Head title='菜篮子' backgroundColor='#0084e7' rightIconContent='删除' showRightIcon='true' rightIconOnClick={ this.HeadDeleteOnclick }></Head>
-        <div style={{ height: 40 }}></div>
-        {this.state.data && this.state.data.length ? this.state.data.map((i, index1) => (
-          <div style={{ backgroundColor: 'white' }}>
-            {this.renderSupplierItem(i, index1)}
-          </div>
-        )) : this.renderEmptyCart()}
-        {this.renderYourLike()}
+        <div className='touch_scroll'>
+          <div style={{ height: 40 }}></div>
+          {this.state.data && this.state.data.length ? this.state.data.map((i, index1) => (
+            <div style={{ backgroundColor: 'white' }}>
+              {this.renderSupplierItem(i, index1)}
+            </div>
+          )) : this.renderEmptyCart()}
+          {this.renderYourLike()}
+        </div>
         {this.state.data && this.state.data.length ? this.renderCartFooter() : <div></div>}
         <div style={{ height: 100 }}></div>
       </div>
