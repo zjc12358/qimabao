0d7c61e9ae885f743f5ddee2d03a96956f3286c2
ywy <631592452@qq.com>
2019-01-11 13:46:23 +0800

    2018/1/11 BUG修复 --by周

--- src/pages/Supplier/index.tsx
+++ src/pages/Supplier/index.tsx
@@ -460,8 +460,7 @@ class Supplier extends React.Component<Props, State> {
   }
 
   public orderOnclick = (index) => {
-    console.log(index)
-    this.props.changeTab(index)
+    this.props.changeTab(index + 1)
     history().push('supplierOrder')
   }
 
